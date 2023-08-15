124b48370e6aaa04a5ed4de23b57597ac5dfa859
Your Name <you@example.com>
2023-08-15 01:35:16 -0700

    twelth commit

---
+++ java1.js
@@ -0,0 +1,47 @@
+
+function openwindow(){
+    let name= "ram";
+alert(`hello ${name}`); 
+
+
+}
+
+function changecolor() {
+    document.getElementById("pera2").style.color="red";
+}
+
+function typebtn() {
+
+    alert(` ${"number"}`)
+}
+
+function typebtn1() {
+
+    alert(` ${"boolean"}`)
+}
+
+function typebtn2() {
+
+    alert(` ${"string"}`)
+}
+
+function typebtn3() {
+
+    alert(` ${"symbol"}`)
+}
+
+function typebtn4() {
+
+    alert(` ${"bigint"}`)
+}
+
+function showage() {
+    let age=prompt('how old are you',18)
+    alert(`yoour age ${age} years old!`)
+}
+
+function comboss() {
+  let boss =  confirm("are you the boss")
+//   alert(boss)
+}
+
